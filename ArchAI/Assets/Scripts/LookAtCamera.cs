45573b652ef948cdbe00be719a386295ffefa238
arihant-awate <76957848+arihant-awate@users.noreply.github.com>
2022-07-21 18:17:24 +0530

    lets see if this works

---
+++ ArchAI/Assets/Scripts/LookAtCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+    [SerializeField] Transform cam;
+    [SerializeField] GameObject camera;
+    void Start()
+    {
+       camera.transform.localRotation= Quaternion.Euler(0f, 90f, 0f);
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localRotation = Quaternion.Euler(0f, cam.eulerAngles.y, 0f);
+    }
+}
---
+++ ArchAI/Assets/Scripts/MoveErika.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveErika : MonoBehaviour
+{
+    [SerializeField]Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            animator.SetBool("IsWalking", true);
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                if (animator.GetBool("IsRunning")==false)
+                {
+                    animator.SetBool("IsWalking", false);
+                    animator.SetBool("IsRunning", true);
+                }
+            }
+            else
+            {
+                animator.SetBool("IsWalking", true);
+
+            }
+            
+            
+                
+            
+           
+        }
+        else
+        {
+            animator.SetBool("IsRunning", false);
+            animator.SetBool("IsWalking", false);
+            
+        }
+    }
+}
