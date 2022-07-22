67d2e25b1949a7bc2bd2510a946d44a972e7c740
arihant-awate <76957848+arihant-awate@users.noreply.github.com>
2022-07-22 16:31:42 +0530

    new update walk  works

--- ArchAI/Assets/Scripts/MoveErika.cs
+++ ArchAI/Assets/Scripts/MoveErika.cs
@@ -4,43 +4,50 @@ using UnityEngine;
 
 public class MoveErika : MonoBehaviour
 {
-    [SerializeField]Animator animator;
+    [SerializeField] Animator animator;
+    [SerializeField] Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W))
         {
+            //rb.AddForce(Vector3.right*2);
+            //animator.SetBool("IsWalking", true);
+
+
             animator.SetBool("IsWalking", true);
+
+            animator.SetBool("IsIdle", false);
             if (Input.GetKey(KeyCode.LeftShift))
             {
-                if (animator.GetBool("IsRunning")==false)
-                {
-                    animator.SetBool("IsWalking", false);
-                    animator.SetBool("IsRunning", true);
-                }
+                
+                animator.SetBool("IsWalking", false);
+                animator.SetBool("IsRunning", true);
             }
             else
             {
                 animator.SetBool("IsWalking", true);
-
+                animator.SetBool("IsRunning", false);
             }
-            
-            
-                
-            
-           
+
+
+
+
         }
         else
         {
-            animator.SetBool("IsRunning", false);
+            //animator.SetBool("IsRunning", false);
             animator.SetBool("IsWalking", false);
-            
+            animator.SetBool("IsIdle", true);
+            animator.SetBool("IsRunning", false);
         }
+        
+
     }
 }
